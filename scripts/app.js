18aaa6454f6c29bae8b2e4e89df4e6e20afbdb2d
Ken <kmartinez@sjcoe.net>
2022-11-28 11:04:03 -0800

    Initial Push

---
+++ scripts/app.js
@@ -0,0 +1,42 @@
+//alert("Welcom to JS");
+
+
+//This is how we would do it in c#
+// string firstName = "Ken";
+// int value = 100;
+// bool fake = true;
+
+
+//primitivge data types
+let firstName = '50 cent';
+let numOne = 35;
+let truth = true;
+
+// undefined
+let notDefined; 
+
+// null - is intentionally abscense of value;
+
+console.log(firstName, numOne);
+console.log(firstName, numOne);
+console.log(truth);
+
+if(firstName == "ken"){
+    console.log("Hello Ken");
+}else{
+    console.log("who are you?");
+}
+
+// for(let i=0; i <=10; i++){
+//     console.log(i);
+// }
+
+let myArray = [];
+myArray.push("Breakfast", "Lunch", "Dinner");
+console.log(myArray)
+console.log(myArray[1]);
+
+let numArray = [1, 3, 5, 434 ,434324 , 293810, "String Value"];
+for(let i=0; i < numArray.length; i++){
+    console.log(numArray[i]);
+}
